cb96b98b6f718260d44b1d920352edd070dfd2cc
Biswajit Sarker <biswajit.sarker@giantmachines.com>
2020-10-26 19:04:10 -0400

    fix default

--- src/reducers/comments.js
+++ src/reducers/comments.js
@@ -1,12 +1,12 @@
-import { SAVE_COMMENT } from '../actions/types';
+import { SAVE_COMMENT } from "../actions/types";
 
-export default function(state = [], action){
-
-    switch(action.type){
-        case SAVE_COMMENT:
-            // return state.concat([action.payload]);
-            // better:
-            return [ ...state, action.payload];
-    }
-    return state;
+export default function (state = [], action) {
+  switch (action.type) {
+    case SAVE_COMMENT:
+      // return state.concat([action.payload]);
+      // better:
+      return [...state, action.payload];
+    default:
+  }
+  return state;
 }
